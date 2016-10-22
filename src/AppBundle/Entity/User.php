ebc6228681473691809a64bacf3d05c3d4b5ba07
Elliot Bouvier <elliotbouvier@yahoo.fr>
2016-10-22 08:13:34 +0400

    Ajout de FOSUserBundle et AliceHautelook

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zneel
+ * Date: 22/10/2016
+ * Time: 06:10
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
